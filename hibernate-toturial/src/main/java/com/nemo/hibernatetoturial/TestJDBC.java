8cd1a13d5c4578ba57e7a8caf90d355f1adf734f
nima ghaferi <nima.ghaferi@gmail.com>
2022-10-14 22:25:38 +0330

    Hibernate first commit

---
+++ hibernate-toturial/src/main/java/com/nemo/hibernatetoturial/TestJDBC.java
@@ -0,0 +1,19 @@
+package com.nemo.hibernatetoturial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+        try {
+            System.out.println("Connect to database: " + jdbcUrl);
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, pass);
+            System.out.println("Connection Successful.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
