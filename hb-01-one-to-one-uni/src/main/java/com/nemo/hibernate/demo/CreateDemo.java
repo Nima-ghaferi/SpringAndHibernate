b56adba04b669f48cac6715e8a81a8622bbacaa9
nima ghaferi <nima.ghaferi@gmail.com>
2022-10-19 21:32:29 +0330

    Hibernate relations-one to one uni

---
+++ hb-01-one-to-one-uni/src/main/java/com/nemo/hibernate/demo/CreateDemo.java
@@ -0,0 +1,34 @@
+package com.nemo.hibernate.demo;
+
+import com.nemo.hibernate.demo.entity.Instructor;
+import com.nemo.hibernate.demo.entity.InstructorDetail;
+import com.nemo.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            Instructor instructor = new Instructor("Chad", "Darby", "chad@gmail.com");
+            InstructorDetail detail = new InstructorDetail("Chad Channel", "Coding");
+            instructor.setInstructorDetail(detail);
+
+            session.beginTransaction();
+            System.out.println("saving instructor:" + instructor);
+            session.save(instructor);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
